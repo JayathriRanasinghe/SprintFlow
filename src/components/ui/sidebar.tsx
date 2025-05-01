884582afde68f8966586075cefa27718ff84b498
Jayathri Ranasinghe <jayathrimr@gmail.com>
2025-05-01 15:27:07 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SidebarContent>
    
    with the text content:
    
        DashboardSprintsTicketsDemo Prep
    
    Relevant files:
    
        - _c18@https://6000-idx-studio-1746104309562.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/src_391a5f6d._.js
        - RootLayout@rsc://React/Server/file:///home/user/studio/.next/server/chunks/ssr/%5Broot%20of%20the%20server%5D__66e4ecaf._.js?2
    
    And change it as follows:
    
    lets add some padding to left and right since 4 items in side bar is reach too much to ends

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -416,7 +416,7 @@ const SidebarContent = React.forwardRef<
       ref={ref}
       data-sidebar="content"
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden px-2", // Added px-2 here
         className
       )}
       {...props}
